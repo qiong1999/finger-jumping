8ef9528bcc044b3929a87a6ccadaa7948688795d
whyroke <2803001950@qq.com>
2021-02-28 18:00:01 +0800

    增加了说明

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
 import Home from './page/home/home';
-
+import './App.css';
 function App() {
     return (
         <div className="App">
+            <div className="header">有任何bug或者意见可联系邮箱whyrove@163.com</div>
             <Home></Home>
         </div>
     );
